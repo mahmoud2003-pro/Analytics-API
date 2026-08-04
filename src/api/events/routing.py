08b189f5dfc86911281c73aaa8a7e062fa3369c3
mahmoud_eslam <mh35177273@gmail.com>
2026-08-04 11:36:31 +0300

    done

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,14 +1,27 @@
 from fastapi import APIRouter
-from .schemas import Eventschema, EventListSchema
+from .schemas import Eventschema, EventListSchema, EventCreateSchema, EventUpdateSchema
 
 router = APIRouter()
 
+# Get Data
 @router.get("/")
 def read_events() -> EventListSchema:
     return {
         "items": [{"id": 1}, {"id": 2}, {"id": 3}]
         }
 
+# Create view
+@router.post("/")
+def create_event(payload: EventCreateSchema) -> Eventschema:
+    print(payload.page)
+    return {"id": 123}
+
 @router.get("/{event_id}")
 def read_event(event_id: int) -> Eventschema:
+    return {"id": event_id}
+
+# Update Data
+@router.put("/{event_id}")
+def update_event(event_id: int, payload:EventUpdateSchema) -> Eventschema:
+    print(payload.description)
     return {"id": event_id}
--- src/api/events/schemas.py
+++ src/api/events/schemas.py
@@ -1,9 +1,20 @@
 from pydantic import BaseModel
 from typing import List
 
+"""
+id
+page
+description
+"""
+
+class EventCreateSchema(BaseModel):
+    page: str
+
+class EventUpdateSchema(BaseModel):
+    description: str
+
 class Eventschema(BaseModel):
     id: int
 
 class EventListSchema(BaseModel):
     items: List[Eventschema]
-
