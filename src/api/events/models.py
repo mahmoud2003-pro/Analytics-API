03ec1fd54bbafb5dc183b961f7a04026f01ba615
mahmoud_eslam <mh35177273@gmail.com>
2026-08-06 12:00:48 +0300

    done

--- src/api/events/models.py
+++ src/api/events/models.py
@@ -1,9 +1,8 @@
 from typing import List, Optional
 from sqlmodel import SQLModel, Field
 
-class Eventschema(SQLModel):
-    # id: Optional[int] = Field(default=None, primary_key=True)
-    id: int
+class EventModel(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     page: Optional[str] = Field(default='')
     description: Optional[str] = Field(default='')
 
@@ -16,4 +15,5 @@ class EventUpdateSchema(SQLModel):
     page: Optional[str] = Field(default='')
 
 class EventListSchema(SQLModel):
-    items: List[Eventschema]
+    items: List[EventModel]
+    count: int
--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,6 +1,6 @@
 import os
 from fastapi import APIRouter
-from .models import Eventschema, EventListSchema, EventCreateSchema, EventUpdateSchema
+from .models import EventModel, EventListSchema, EventCreateSchema, EventUpdateSchema
 from api.events.db.config import DATABASE_URL
 
 router = APIRouter()
@@ -15,18 +15,18 @@ def read_events() -> EventListSchema:
 
 # Create view
 @router.post("/")
-def create_event(payload: EventCreateSchema) -> Eventschema:
+def create_event(payload: EventCreateSchema) -> EventModel:
     print(payload.page)
     data = payload.model_dump() # payload -> dict -> paydantic
     return {"id": 123, **data}
 
 @router.get("/{event_id}")
-def read_event(event_id: int) -> Eventschema:
+def read_event(event_id: int) -> EventModel:
     return {"id": event_id}
 
 # Update Data
 @router.put("/{event_id}")
-def update_event(event_id: int, payload:EventUpdateSchema) -> Eventschema:
+def update_event(event_id: int, payload:EventUpdateSchema) -> EventModel:
     print(payload.description)
     data = payload.model_dump()
     return {"id": event_id, **data}
